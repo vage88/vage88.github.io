355ed3bf175b082f984f505bb270f38cec14e868
Vage77 <51483482+Vage77@users.noreply.github.com>
2019-06-17 22:32:11 +0400

    1

---
+++ 11.js
@@ -0,0 +1,407 @@
+document.addEventListener("DOMContentLoaded", function (event) {
+    init();
+});
+
+function init() {
+
+    var c = 0;
+
+            function Engine(num) {
+                if (num == 0) { c++ }
+                else { var obj = document.getElementById((num)); obj.innerHTML = (num) }
+            }
+            function ca() {
+                // Находим нужный элемент по ID
+                var obj = document.getElementById('Txt');
+                // Меняем его содержимое
+                obj.innerHTML = (x);
+            }
+            function show(divid) {
+                if (document.getElementById(divid).style.display == "none") {
+                    document.getElementById(divid).style.display = "inline";
+                }
+                else {
+                    document.getElementById(divid).style.display = "none";
+                }
+            }
+            function otv() {
+                var score = c; x = score
+                {
+                    if (score <= 100) {
+                        document.getElementById(divid = "otv1").style.display = "inline";
+                    }
+
+                    {
+                        if (score >= 100 && score <= 110) {
+                            document.getElementById(divid = "otv2").style.display = "inline";
+                        }
+
+                        {
+                            if (score >= 110 && score <= 200) {
+                                document.getElementById(divid = "otv3").style.display = "inline";
+                            }
+
+                            {
+                                if (score >= 210 && score <= 300) {
+                                    document.getElementById(divid = "otv4").style.display = "inline";
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            $(document).ready(function () {
+                function hidetext() 
+                {
+                    $("#text").hide(1000, function () {
+                        $("#hide").hide();
+                        $("#show").show();
+                    });
+                }
+                function showtext() {
+                    $("#text").show(1000, function () {
+                        $("#hide").show();
+                        $("#show").hide();
+                    });
+                }
+                function animation() {
+                    $("#text").hide(1000, function () {
+                        $("#hide").fadeOut();
+                        $("#show").show();
+                    });
+                }
+                $("#hide").bind("click", hidetext);
+                $("#show").bind("click", showtext);
+                $("#hide").bind("click", animation);
+
+            })
+
+            $(document).ready(function () {
+
+
+        })
+
+
+        $(document).ready(function () {
+            $("#hide1").bind("click", hidetext);
+                $("#show1").bind("click", showtext);
+                $("#hide1").bind("click", animation);
+               
+                function hidetext() 
+                {
+                    $("#text1").hide(1000, function () {
+                        $("#hide1").hide();
+                        $("#show1").show();
+                    });
+                }
+                function showtext() {
+                    $("#text1").show(1000, function () {
+                        $("#hide1").show();
+                        $("#show1").hide();
+                    });
+                }
+                function animation() {
+                    $("#text1").hide(1000, function () {
+                        $("#hide1").fadeOut();
+                        $("#show1").show();
+                    });
+                }
+
+                
+})
+
+
+$(document).ready(function () {
+
+function hidetext() 
+                {
+                    $("#text2").hide(1000, function () {
+                        $("#hide2").hide();
+                        $("#show2").show();
+                    });
+                }
+                function showtext() {
+                    $("#text2").show(1000, function () {
+                        $("#hide2").show();
+                        $("#show2").hide();
+                    });
+                }
+                function animation() {
+                    $("#text2").hide(1000, function () {
+                        $("#hide2").fadeOut();
+                        $("#show2").show();
+                    });
+                }
+
+$("#hide2").bind("click", hidetext);
+                $("#show2").bind("click", showtext);
+                $("#hide2").bind("click", animation);
+                
+})
+
+
+$(document).ready(function () {
+
+function hidetext() 
+                {
+                    $("#text3").hide(1000, function () {
+                        $("#hide3").hide();
+                        $("#show3").show();
+                    });
+                }
+                function showtext() {
+                    $("#text3").show(1000, function () {
+                        $("#hide3").show();
+                        $("#show3").hide();
+                    });
+                }
+                function animation() {
+                    $("#text3").hide(1000, function () {
+                        $("#hide3").fadeOut();
+                        $("#show3").show();
+                    });
+                }
+$("#hide3").bind("click", hidetext);
+                $("#show3").bind("click", showtext);
+                $("#hide3").bind("click", animation);
+                
+})
+
+
+$(document).ready(function () {
+function hidetext() 
+                {
+                    $("#text4").hide(1000, function () {
+                        $("#hide4").hide();
+                        $("#show4").show();
+                    });
+                }
+                function showtext() {
+                    $("#text4").show(1000, function () {
+                        $("#hide4").show();
+                        $("#show4").hide();
+                    });
+                }
+                function animation() {
+                    $("#text4").hide(1000, function () {
+                        $("#hide4").fadeOut();
+                        $("#show4").show();
+                    });
+                }
+$("#hide4").bind("click", hidetext);
+                $("#show4").bind("click", showtext);
+                $("#hide4").bind("click", animation);           
+})
+
+
+$(document).ready(function () {
+function hidetext() 
+                {
+                    $("#text5").hide(1000, function () {
+                        $("#hide5").hide();
+                        $("#show5").show();
+                    });
+                }
+                function showtext() {
+                    $("#text5").show(1000, function () {
+                        $("#hide5").show();
+                        $("#show5").hide();
+                    });
+                }
+                function animation() {
+                    $("#text5").hide(1000, function () {
+                        $("#hide5").fadeOut();
+                        $("#show5").show();
+                    });
+                }
+
+$("#hide5").bind("click", hidetext);
+                $("#show5").bind("click", showtext);
+                $("#hide5").bind("click", animation);
+                
+})
+
+
+$(document).ready(function () {
+
+
+function hidetext() 
+                {
+                    $("#text6").hide(1000, function () {
+                        $("#hide6").hide();
+                        $("#show6").show();
+                    });
+                }
+                function showtext() {
+                    $("#text6").show(1000, function () {
+                        $("#hide6").show();
+                        $("#show6").hide();
+                    });
+                }
+                function animation() {
+                    $("#text6").hide(1000, function () {
+                        $("#hide6").fadeOut();
+                        $("#show6").show();
+                    });
+                }
+$("#hide6").bind("click", hidetext);
+                $("#show6").bind("click", showtext);
+                $("#hide6").bind("click", animation);    
+})
+
+
+
+$(document).ready(function () {
+
+
+function hidetext() 
+                {
+                    $("#text7").hide(1000, function () {
+                        $("#hide7").hide();
+                        $("#show7").show();
+                    });
+                }
+                function showtext() {
+                    $("#text7").show(1000, function () {
+                        $("#hide7").show();
+                        $("#show7").hide();
+                    });
+                }
+                function animation() {
+                    $("#text7").hide(1000, function () {
+                        $("#hide7").fadeOut();
+                        $("#show7").show();
+                    });
+                }
+
+$("#hide7").bind("click", hidetext);
+                $("#show7").bind("click", showtext);
+                $("#hide7").bind("click", animation);    
+})
+
+
+
+
+$(document).ready(function () {
+
+
+function hidetext() 
+                {
+                    $("#text8").hide(1000, function () {
+                        $("#hide8").hide();
+                        $("#show8").show();
+                    });
+                }
+                function showtext() {
+                    $("#text8").show(1000, function () {
+                        $("#hide8").show();
+                        $("#show8").hide();
+                    });
+                }
+                function animation() {
+                    $("#text8").hide(1000, function () {
+                        $("#hide8").fadeOut();
+                        $("#show8").show();
+                    });
+                }
+
+$("#hide8").bind("click", hidetext);
+                $("#show8").bind("click", showtext);
+                $("#hide8").bind("click", animation);
+})
+
+
+
+
+
+$(document).ready(function () {
+
+function hidetext() 
+                {
+                    $("#text9").hide(1000, function () {
+                        $("#hide9").hide();
+                        $("#show9").show();
+                    });
+                }
+                function showtext() {
+                    $("#text9").show(1000, function () {
+                        $("#hide9").show();
+                        $("#show9").hide();
+                    });
+                }
+                function animation() {
+                    $("#text9").hide(1000, function () {
+                        $("#hide9").fadeOut();
+                        $("#show9").show();
+                    });
+                }
+
+
+
+$("#hide9").bind("click", hidetext);
+                $("#show9").bind("click", showtext);
+                $("#hide9").bind("click", animation);
+
+
+                
+})
+
+
+
+
+
+$(document).ready(function () {
+
+function hidetext() 
+                {
+                    $("#text10").hide(1000, function () {
+                        $("#hide10").hide();
+                        $("#show10").show();
+                    });
+                }
+                function showtext() {
+                    $("#text10").show(1000, function () {
+                        $("#hide10").show();
+                        $("#show10").hide();
+                    });
+                }
+                function animation() {
+                    $("#text10").hide(1000, function () {
+                        $("#hide10").fadeOut();
+                        $("#show10").show();
+                    });
+                }
+                $("#hide10").bind("click", hidetext);
+                $("#show10").bind("click", showtext);
+                $("#hide10").bind("click", animation);
+                
+})
+
+                $(document).ready(function () {
+
+                function hidetext() 
+                {
+                    $("#text11").hide(1000, function () {
+                        $("#hide11").hide();
+                        $("#show11").show();
+                    });
+                }
+                function showtext() {
+                    $("#text11").show(1000, function () {
+                        $("#hide11").show();
+                        $("#show11").hide();
+                    });
+                }
+                function animation() {
+                    $("#text11").hide(1000, function () {
+                        $("#hide11").fadeOut();
+                        $("#show11").show();
+                    });
+                }
+
+                $("#hide11").bind("click", hidetext);
+                $("#show11").bind("click", showtext);
+                $("#hide11").bind("click", animation);
+            })
+}
